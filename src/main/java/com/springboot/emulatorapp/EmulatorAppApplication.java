828687749489cb224d4229b182e718b51c572981
Alena Zhilina <alen.zhilina@gmail.com>
2022-04-17 00:49:47 +0300

    Initial project version

---
+++ src/main/java/com/springboot/emulatorapp/EmulatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.emulatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmulatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmulatorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/emulatorapp/EmulatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.emulatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmulatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
